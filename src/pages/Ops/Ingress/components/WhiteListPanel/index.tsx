d20a7b26a9d87250958f95e83b17f402a431c0c7
Marcus Lin <linfaimom@gmail.com>
2024-06-01 17:56:42 +0800

    feat: 加入全部清空的逻辑

--- src/pages/Ops/Ingress/components/WhiteListPanel/index.tsx
+++ src/pages/Ops/Ingress/components/WhiteListPanel/index.tsx
@@ -1,5 +1,5 @@
 import { fetchIngressWhiteList, updateIngressWhiteList } from '@/services/ingress/api';
-import { SaveFilled } from '@ant-design/icons';
+import { DeleteOutlined, PlusOutlined, SaveFilled } from '@ant-design/icons';
 import { useRequest } from '@umijs/max';
 import {
   Card,
@@ -24,18 +24,23 @@ type WhiteListPanelProps = {
 };
 
 const WhiteListPanel: React.FC<WhiteListPanelProps> = (props) => {
-  const [messageApi, contextHolder] = message.useMessage();
+  const [messageApi, msgContextHolder] = message.useMessage();
   const [drawerOpened, setDrawerOpened] = useState<boolean>(false);
   const [confirmModalOpened, setConfirmModalOpened] = useState<boolean>(false);
   const [editEnabled, setEditEnabled] = useState<boolean>(false);
   const [ipAddrs, setIpAddrs] = useState<string[]>([]);
   const [addedIpAddrs, setAddedIpAddrs] = useState<string[]>([]);
   const [removedIpAddrs, setRemovedIpAddrs] = useState<string[]>([]);
-  const { data, run } = useRequest(() => fetchIngressWhiteList({ ...props }), { manual: true });
-  const { run: runPostData, error } = useRequest(
-    () => updateIngressWhiteList({ ipList: ipAddrs, override: true, ...props }),
-    { manual: true },
-  );
+  const { data, loading, run } = useRequest(() => fetchIngressWhiteList({ ...props }), {
+    manual: true,
+  });
+  const {
+    loading: postDataLoading,
+    run: runPostData,
+    error,
+  } = useRequest(() => updateIngressWhiteList({ ipList: ipAddrs, override: true, ...props }), {
+    manual: true,
+  });
   useEffect(() => {
     if (data && Array.isArray(data)) {
       setIpAddrs(data);
@@ -45,15 +50,23 @@ const WhiteListPanel: React.FC<WhiteListPanelProps> = (props) => {
     setDrawerOpened(true);
     run();
   };
+  const toggleOpenEditMode = (open: boolean) => {
+    if (!open && (addedIpAddrs.length > 0 || removedIpAddrs.length > 0)) {
+      alert('所有变更将被还原');
+      setIpAddrs(data as string[]);
+      setAddedIpAddrs([]);
+      setRemovedIpAddrs([]);
+    }
+    setEditEnabled(open);
+  };
   const closeDrawer = () => {
+    if (editEnabled) {
+      toggleOpenEditMode(false);
+    }
     setDrawerOpened(false);
-    setEditEnabled(false);
     setIpAddrs([]);
     setAddedIpAddrs([]);
     setRemovedIpAddrs([]);
-  };
-  const toggleOpenEditMode = (open: boolean) => {
-    setEditEnabled(open);
   };
   const handleRemoveIpAddr = (removedIpAddr: string) => {
     setIpAddrs(ipAddrs.filter((ip) => ip !== removedIpAddr));
@@ -63,104 +76,123 @@ const WhiteListPanel: React.FC<WhiteListPanelProps> = (props) => {
   const previewChangesAndConfirm = () => {
     setConfirmModalOpened(true);
   };
+  const submitChanges = async () => {
+    await runPostData();
+    if (error) {
+      messageApi.open({
+        type: 'error',
+        content: error.message,
+      });
+    } else {
+      messageApi.open({
+        type: 'success',
+        content: '更新成功',
+      });
+    }
+    setConfirmModalOpened(false);
+    setEditEnabled(false);
+    setAddedIpAddrs([]);
+    setRemovedIpAddrs([]);
+  };
   return (
     <div>
-      {contextHolder}
+      {msgContextHolder}
       <a onClick={showDrawer}>白名单</a>
-      <Drawer size="large" title={props.name} onClose={closeDrawer} open={drawerOpened}>
-        {Array.isArray(data) && (
-          <>
-            <Row gutter={16}>
-              <Col span={8}>
-                <Card>
-                  <Statistic
-                    title="总计"
-                    value={ipAddrs.length}
-                    valueStyle={{ color: '#3f8600' }}
-                  />
-                </Card>
-              </Col>
-              <Col span={8}>
-                <Card>
-                  <Statistic
-                    title="IP 数"
-                    value={ipAddrs.length - ipAddrs.filter((ip: string) => ip.includes('/')).length}
-                  />
-                </Card>
-              </Col>
-              <Col span={8}>
-                <Card>
-                  <Statistic
-                    title="CIDR 数"
-                    value={ipAddrs.filter((ip: string) => ip.includes('/')).length}
-                  />
-                </Card>
-              </Col>
-            </Row>
-            <Divider plain={true}>
-              编辑模式 <Switch onChange={toggleOpenEditMode} checked={editEnabled} />
-            </Divider>
-            <Flex gap="4px 0" wrap>
-              {ipAddrs.map((ip: string) => {
-                return (
-                  <Tag
-                    closable={editEnabled}
-                    key={ip}
-                    color={ip.includes('/') ? 'orange' : 'blue'}
-                    onClose={() => handleRemoveIpAddr(ip)}
-                  >
-                    {ip}
-                  </Tag>
-                );
-              })}
-            </Flex>
-            {editEnabled && (addedIpAddrs.length > 0 || removedIpAddrs.length > 0) && (
-              <FloatButton
-                shape="circle"
-                type="primary"
-                icon={<SaveFilled />}
-                tooltip="预览并保存"
-                onClick={previewChangesAndConfirm}
+      <Drawer
+        size="large"
+        title={props.name}
+        onClose={closeDrawer}
+        open={drawerOpened}
+        loading={loading}
+      >
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card>
+              <Statistic title="总计" value={ipAddrs.length} valueStyle={{ color: '#3f8600' }} />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card>
+              <Statistic
+                title="IP 数"
+                value={ipAddrs.length - ipAddrs.filter((ip: string) => ip.includes('/')).length}
               />
-            )}
-            <Modal
-              title="变动预览"
-              open={confirmModalOpened}
-              onOk={async () => {
-                await runPostData();
-                if (error) {
-                  messageApi.open({
-                    type: 'error',
-                    content: error.message,
-                  });
-                } else {
-                  messageApi.open({
-                    type: 'success',
-                    content: '更新成功',
-                  });
-                }
-                setConfirmModalOpened(false);
-                setEditEnabled(false);
-                setAddedIpAddrs([]);
-                setRemovedIpAddrs([]);
-              }}
-              onCancel={() => {
-                setConfirmModalOpened(false);
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card>
+              <Statistic
+                title="CIDR 数"
+                value={ipAddrs.filter((ip: string) => ip.includes('/')).length}
+              />
+            </Card>
+          </Col>
+        </Row>
+        <Divider plain={true}>
+          编辑模式 <Switch onChange={toggleOpenEditMode} checked={editEnabled} />
+        </Divider>
+        <Flex gap="4px 0" wrap>
+          {editEnabled && (
+            <Tag
+              color="blue"
+              onClick={() => {
+                alert('plus');
               }}
             >
-              <p>删除的内容（共 {removedIpAddrs.length} 条）</p>
-              {removedIpAddrs.map((ip: string) => {
-                return (
-                  <Tag key={ip} color={'red'}>
-                    {ip}
-                  </Tag>
-                );
-              })}
-              <Divider />
-              <p>新增的内容（共 {addedIpAddrs.length} 条）</p>
-            </Modal>
-          </>
+              <PlusOutlined />
+              批量新增
+            </Tag>
+          )}
+          {editEnabled && ipAddrs.length > 0 && (
+            <Tag
+              color="red"
+              onClick={() => {
+                setRemovedIpAddrs(ipAddrs);
+                setIpAddrs([]);
+              }}
+            >
+              <DeleteOutlined />
+              全部清空
+            </Tag>
+          )}
+          {ipAddrs.map((ip: string) => {
+            return (
+              <Tag closable={editEnabled} key={ip} onClose={() => handleRemoveIpAddr(ip)}>
+                {ip}
+              </Tag>
+            );
+          })}
+        </Flex>
+        {editEnabled && (addedIpAddrs.length > 0 || removedIpAddrs.length > 0) && (
+          <FloatButton
+            shape="circle"
+            type="primary"
+            icon={<SaveFilled />}
+            tooltip="预览并保存"
+            onClick={previewChangesAndConfirm}
+          />
         )}
+        <Modal
+          title="变动预览"
+          open={confirmModalOpened}
+          okText={'确认提交'}
+          onOk={submitChanges}
+          confirmLoading={postDataLoading}
+          onCancel={() => {
+            setConfirmModalOpened(false);
+          }}
+        >
+          <p>删除的内容（共 {removedIpAddrs.length} 条）</p>
+          {removedIpAddrs.map((ip: string) => {
+            return (
+              <Tag key={ip} color={'red'}>
+                {ip}
+              </Tag>
+            );
+          })}
+          <Divider />
+          <p>新增的内容（共 {addedIpAddrs.length} 条）</p>
+        </Modal>
       </Drawer>
     </div>
   );
