9feef37c35c4a998d9041f7c33598a44fbe8798c
Marcus Lin <linfaimom@gmail.com>
2024-06-03 23:14:51 +0800

    fix: 移除不必要的泛型

--- src/services/env/api.ts
+++ src/services/env/api.ts
@@ -2,7 +2,7 @@ import { request } from '@umijs/max';
 
 /** 获取 env 列表 GET /api/v1/envs */
 export async function fetchEnvs(options?: { [key: string]: any }) {
-  return request<API.Env[]>('/api/v1/envs', {
+  return request('/api/v1/envs', {
     method: 'GET',
     ...(options || {}),
   });
