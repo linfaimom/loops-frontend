16086e08a72c434342b5b6a97545cbf65259b184
Marcus Lin <linfaimom@gmail.com>
2024-06-03 23:11:54 +0800

    fix: 修复 envId 传参问题

--- src/pages/Ops/Ingress/index.tsx
+++ src/pages/Ops/Ingress/index.tsx
@@ -1,8 +1,7 @@
 import { fetchEnvs } from '@/services/env/api';
 import { fetchIngresses } from '@/services/ingress/api';
 import { fetchNamespaces } from '@/services/namespace/api';
-import type { ProColumns } from '@ant-design/pro-components';
-import { PageContainer, ProTable, TableDropdown } from '@ant-design/pro-components';
+import { PageContainer, ProColumns, ProTable, TableDropdown } from '@ant-design/pro-components';
 import '@umijs/max';
 import { useRequest } from '@umijs/max';
 import { Popover, Tabs } from 'antd';
@@ -18,112 +17,112 @@ const listNamespaces = async (params: { envId: number }) => {
   });
 };
 
-const columns: ProColumns<API.Ingress>[] = [
-  {
-    dataIndex: 'index',
-    valueType: 'index',
-  },
-  {
-    title: '命名空间',
-    dataIndex: 'namespace',
-    initialValue: 'low-code',
-    valueType: 'select',
-    params: { envId: 1 },
-    request: listNamespaces,
-  },
-  {
-    title: 'ingress 类',
-    search: false,
-    dataIndex: 'ingressClass',
-  },
-  {
-    title: 'ingress 名称',
-    dataIndex: 'ingressName',
-    copyable: true,
-    search: false,
-  },
-  {
-    title: '域名',
-    dataIndex: 'domain',
-    render: (_, record: API.Ingress) => {
-      let hosts = Object.keys(record.rules);
-      return (
-        <div>
-          {hosts.map((host, index) => {
-            let rule = () => {
-              return (
-                <div>
-                  {record.rules[host].map((rule, i) => {
-                    return <p key={i}>{rule}</p>;
-                  })}
-                </div>
-              );
-            };
-            return (
-              <Popover key={index} content={rule}>
-                <p>
-                  {host.includes('*') ? (
-                    host
-                  ) : (
-                    <a href={'http://' + host} target="_blank" rel="noreferrer">
-                      {host}
-                    </a>
-                  )}
-                </p>
-              </Popover>
-            );
-          })}
-        </div>
-      );
-    },
-  },
-  {
-    title: '操作',
-    search: false,
-    valueType: 'option',
-    render: (_, record, index) => {
-      return [
-        <WhiteListPanel
-          key={index}
-          envId={record.envId}
-          namespace={record.namespace}
-          name={record.ingressName}
-        />,
-        <IngressYamlViewer
-          key={index}
-          envId={record.envId}
-          namespace={record.namespace}
-          name={record.ingressName}
-        />,
-        <TableDropdown
-          key="actionGroup"
-          menus={[{ key: 'editAnnotations', name: '配置 Annotations' }]}
-          onSelect={(key: string) => {
-            switch (key) {
-              case 'editAnnotations':
-                alert('edit annotations');
-                break;
-              default:
-                break;
-            }
-          }}
-        />,
-      ];
-    },
-  },
-];
-
 const IngressList: React.FC = () => {
   const { data } = useRequest(() => fetchEnvs());
   const [envId, setEnvId] = useState<number>(0);
+  const columns: ProColumns<API.Ingress>[] = [
+    {
+      dataIndex: 'index',
+      valueType: 'index',
+    },
+    {
+      title: '命名空间',
+      dataIndex: 'namespace',
+      initialValue: 'low-code',
+      valueType: 'select',
+      params: { envId: envId },
+      request: listNamespaces,
+    },
+    {
+      title: 'ingress 类',
+      search: false,
+      dataIndex: 'ingressClass',
+    },
+    {
+      title: 'ingress 名称',
+      dataIndex: 'ingressName',
+      copyable: true,
+      search: false,
+    },
+    {
+      title: '域名',
+      dataIndex: 'domain',
+      render: (_, record: API.Ingress) => {
+        let hosts = Object.keys(record.rules);
+        return (
+          <div>
+            {hosts.map((host, index) => {
+              let rules = () => {
+                return (
+                  <div>
+                    {record.rules[host].map((rule, i) => {
+                      return <p key={i}>{rule}</p>;
+                    })}
+                  </div>
+                );
+              };
+              return (
+                <Popover key={index} content={rules}>
+                  <p>
+                    {host.includes('*') ? (
+                      host
+                    ) : (
+                      <a href={'http://' + host} target="_blank" rel="noreferrer">
+                        {host}
+                      </a>
+                    )}
+                  </p>
+                </Popover>
+              );
+            })}
+          </div>
+        );
+      },
+    },
+    {
+      title: '操作',
+      search: false,
+      valueType: 'option',
+      render: (_, record, index) => {
+        return [
+          <WhiteListPanel
+            key={index}
+            envId={record.envId}
+            namespace={record.namespace}
+            name={record.ingressName}
+          />,
+          <IngressYamlViewer
+            key={index}
+            envId={record.envId}
+            namespace={record.namespace}
+            name={record.ingressName}
+          />,
+          <TableDropdown
+            key="actionGroup"
+            menus={[{ key: 'editAnnotations', name: '配置 Annotations' }]}
+            onSelect={(key: string) => {
+              switch (key) {
+                case 'editAnnotations':
+                  alert('edit annotations');
+                  break;
+                default:
+                  break;
+              }
+            }}
+          />,
+        ];
+      },
+    },
+  ];
   useEffect(() => {
     if (data && Array.isArray(data) && data.length > 0) {
       setEnvId(data[0].id);
     }
   }, [data]);
+
   return (
     <PageContainer>
-      {Array.isArray(data) && (
+      {Array.isArray(data) && envId > 0 && (
         <>
           <Tabs
             type="card"
