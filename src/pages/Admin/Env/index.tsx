6d5bb275e08c4cb779a37f316458074cf3e4ad3a
林凤翔 <linfengxiang01@corp.netease.com>
2024-06-06 23:51:40 +0800

    feat: 增删 env 的处理

--- src/pages/Admin/Env/index.tsx
+++ src/pages/Admin/Env/index.tsx
@@ -1,8 +1,24 @@
-import { fetchEnvs } from '@/services/env/api';
-import { PageContainer, ProColumns, ProTable } from '@ant-design/pro-components';
-import { Button } from 'antd';
+import { createEnv, deleteEnv, fetchEnvs } from '@/services/env/api';
+import { PlusOutlined } from '@ant-design/icons';
+import {
+  ActionType,
+  DrawerForm,
+  PageContainer,
+  ProColumns,
+  ProForm,
+  ProFormText,
+  ProFormTextArea,
+  ProTable,
+} from '@ant-design/pro-components';
+import { Button, Popconfirm } from 'antd';
+import { useRef, useState } from 'react';
 
 const EnvList: React.FC = () => {
+  const [drawerOpened, setDrawerOpened] = useState<boolean>(false);
+  const actionRef = useRef<ActionType>();
+  const showDrawer = () => {
+    setDrawerOpened(true);
+  };
   const columns: ProColumns<API.Env>[] = [
     {
       title: '环境 id',
@@ -21,15 +37,101 @@ const EnvList: React.FC = () => {
       dataIndex: 'agentUrl',
       copyable: true,
     },
+    {
+      title: '操作',
+      search: false,
+      valueType: 'option',
+      render: (_, record, index) => {
+        return [
+          <Popconfirm
+            key={index}
+            title="删除环境信息"
+            description="请确认是否删除该环境信息？"
+            onConfirm={async () => {
+              await deleteEnv({ id: record.id });
+              actionRef.current?.reload();
+            }}
+          >
+            <a>删除</a>
+          </Popconfirm>,
+        ];
+      },
+    },
   ];
   return (
     <PageContainer>
       <ProTable
+        actionRef={actionRef}
         search={false}
         columns={columns}
         request={fetchEnvs}
-        headerTitle={<Button type="primary">新建环境</Button>}
+        headerTitle={
+          <Button type="primary" onClick={showDrawer} icon={<PlusOutlined />}>
+            新建环境
+          </Button>
+        }
       />
+      <DrawerForm
+        open={drawerOpened}
+        onOpenChange={setDrawerOpened}
+        onFinish={async (formData) => {
+          console.log(formData);
+          //@ts-ignore
+          await createEnv({ ...formData });
+          actionRef.current?.reload();
+          return true;
+        }}
+        title="新建环境"
+      >
+        <ProFormText
+          required
+          name="name"
+          label="环境名称"
+          rules={[
+            {
+              required: true,
+              message: '请输入环境名',
+            },
+          ]}
+        />
+        <ProFormTextArea name="description" label="环境描述" />
+        <ProFormText
+          required
+          name="agentUrl"
+          label="Agent 地址"
+          placeholder="形如 https://loops-agent.xxxx.com"
+          rules={[
+            {
+              required: true,
+              message: '请输入 Agent 地址',
+            },
+          ]}
+        />
+        <ProForm.Group>
+          <ProFormText
+            name="agentAuthUser"
+            width="md"
+            label="Agent 认证账号"
+            rules={[
+              {
+                required: true,
+                message: '请输入 Agent 认证账号',
+              },
+            ]}
+          />
+          <ProFormText.Password
+            name="agentAuthPasswd"
+            width="md"
+            label="Agent 认证密码"
+            rules={[
+              {
+                required: true,
+                message: '请输入 Agent 认证密码',
+              },
+            ]}
+          />
+        </ProForm.Group>
+      </DrawerForm>
     </PageContainer>
   );
 };
--- src/services/env/api.ts
+++ src/services/env/api.ts
@@ -7,3 +7,30 @@ export async function fetchEnvs(options?: { [key: string]: any }) {
     ...(options || {}),
   });
 }
+
+type EnvCreateReqProps = {
+  name: string;
+  description: string;
+  agentUrl: string;
+  agentAuthUser: string;
+  agentAuthPasswd: string;
+};
+
+/** 创建 env 信息 POST /api/v1/envs */
+export async function createEnv(params: EnvCreateReqProps, options?: { [key: string]: any }) {
+  return request<API.CommonResponse<number>>('/api/v1/envs', {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** 删除 env 信息 DELETE /api/v1/envs */
+export async function deleteEnv(params: { id: number }, options?: { [key: string]: any }) {
+  return request<API.CommonResponse<boolean>>('/api/v1/envs/' + params.id, {
+    method: 'DELETE',
+    ...(options || {}),
+  });
+}
