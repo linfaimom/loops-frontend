7f6d010b15322d5827872a897af769801b282c12
林凤翔 <linfengxiang01@corp.netease.com>
2024-07-08 18:50:36 +0800

    feat: 支持对比灰度/正式空间下的镜像情况

--- src/pages/Dev/EnvDeployInfo/index.tsx
+++ src/pages/Dev/EnvDeployInfo/index.tsx
@@ -2,19 +2,93 @@ import { fetchDeploymentBasicInfos } from '@/services/deployment/api';
 import { fetchEnvs } from '@/services/env/api';
 import { PageContainer, ProColumns, ProTable } from '@ant-design/pro-components';
 import { useRequest } from '@umijs/max';
-import { Switch, Tabs } from 'antd';
+import { Button, Modal, Space, Switch, Tabs } from 'antd';
 import { useEffect, useState } from 'react';
+import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer-continued';
 
 const formalNamespace = 'low-code';
 const grayNamespace = 'gray-low-code';
+const components = [
+  'lcap',
+  'lcap-fe',
+  'nasl-generator',
+  'nasl-generator-new',
+  'nasl-generator-fe',
+  'nasl-storage',
+  'owl',
+  'nuims',
+  'nl2nasl',
+  'nl2nasl-backend',
+  'codewave-insight',
+  'lcap-debugger',
+];
 
 const EnvDeployInfo: React.FC = () => {
   const { data } = useRequest(() => fetchEnvs());
   const [envId, setEnvId] = useState<number>(0);
   const [envName, setEnvName] = useState<string>('');
   const [namespace, setNamespace] = useState<string>(formalNamespace);
+  const [modal, contextHolder] = Modal.useModal();
   const toggleNamespace = (checked: boolean) => {
     setNamespace(checked ? formalNamespace : grayNamespace);
+  };
+  const showNamespaceDiffView = async () => {
+    // 正式命名空间的信息
+    let formalInfoResp = await fetchDeploymentBasicInfos({
+      envId: envId,
+      namespace: formalNamespace,
+    });
+    let formalInfo = formalInfoResp.data;
+    // 灰度命名空间的信息
+    let grayInfoResp = await fetchDeploymentBasicInfos({
+      envId: envId,
+      namespace: grayNamespace,
+    });
+    let grayInfo = grayInfoResp.data;
+    // 仅比对关注的组件即可
+    let grayInfoTxt = grayInfo
+      .map((item) => {
+        if (components.includes(item.deploymentName)) {
+          let containerNames = Object.keys(item.containers);
+          let images = containerNames.map((name) => {
+            return item.containers[name];
+          });
+          let imagesTxt = images.join(';');
+          return item.deploymentName + ' => ' + imagesTxt;
+        }
+        return '';
+      })
+      .filter((v) => v !== '')
+      .sort()
+      .join('\n\n');
+    let formalInfoTxt = formalInfo
+      .map((item) => {
+        if (components.includes(item.deploymentName)) {
+          let containerNames = Object.keys(item.containers);
+          let images = containerNames.map((name) => {
+            return item.containers[name];
+          });
+          let imagesTxt = images.join(';');
+          return item.deploymentName + ' => ' + imagesTxt;
+        }
+        return '';
+      })
+      .filter((v) => v !== '')
+      .sort()
+      .join('\n\n');
+    // 展示 Diff
+    modal.info({
+      width: 2000,
+      content: (
+        <ReactDiffViewer
+          leftTitle={'灰度命名空间'}
+          oldValue={grayInfoTxt}
+          rightTitle={'正式命名空间'}
+          newValue={formalInfoTxt}
+          compareMethod={DiffMethod.LINES}
+        />
+      ),
+    });
   };
   const columns: ProColumns<API.DeploymentWithContainers>[] = [
     {
@@ -52,6 +126,7 @@ const EnvDeployInfo: React.FC = () => {
   }, [data]);
   return (
     <PageContainer>
+      {contextHolder}
       {data && envId > 0 && (
         <>
           <Tabs
@@ -77,12 +152,17 @@ const EnvDeployInfo: React.FC = () => {
             request={fetchDeploymentBasicInfos}
             headerTitle={
               (envName.includes('drill') || envName.includes('inner')) && (
-                <Switch
-                  onChange={toggleNamespace}
-                  checked={namespace === formalNamespace}
-                  checkedChildren="正式命名空间"
-                  unCheckedChildren="灰度命名空间"
-                />
+                <Space>
+                  <Switch
+                    onChange={toggleNamespace}
+                    checked={namespace === formalNamespace}
+                    checkedChildren="正式命名空间"
+                    unCheckedChildren="灰度命名空间"
+                  />
+                  <Button size="small" onClick={() => showNamespaceDiffView()}>
+                    对比命名空间镜像差异
+                  </Button>
+                </Space>
               )
             }
           />
